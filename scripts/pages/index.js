70e29c3a7928dc53382819754b4dcd65dbbf0a1f
laetitiaCP <104037559+laetitiaCP@users.noreply.github.com>
2023-04-17 00:30:55 +0200

    Add files via upload

---
+++ scripts/pages/index.js
@@ -0,0 +1,33 @@
+   
+     
+    async function getPhotographers() {
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+        const dataPhotographers = fetch('https://server.com/data/photographers.json')
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+
+       
+
+        // et bien retourner le tableau photographers seulement une fois récupéré
+        return console.log(dataPhotographers);
+    }
+
+    async function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer_section");
+
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
+        });
+    };
+
+    async function init() {
+        // Récupère les datas des photographes
+        const { photographers } = await getPhotographers();
+        displayData(photographers);
+    };
+    
+    init();
+    
